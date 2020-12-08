500e239b5e996e92ddc23c26a0f90f54ed9af0c7
David <deh0_@hotmail.com>
2020-12-08 16:55:54 -0300

    Corrigido identação

--- src/main/java/br/com/david/testeinsight/model/WorkingHours.java
+++ src/main/java/br/com/david/testeinsight/model/WorkingHours.java
@@ -4,7 +4,6 @@ package br.com.david.testeinsight.model;
  * Classe modelo para representar Horário de trabalho
  * @author David Arruda
  */
-public class WorkingHours extends AbstractModel{
-    
+public class WorkingHours extends AbstractModel{ 
     
 }
