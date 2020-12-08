4f19c7acaed753a0e788942d1e22fb3d535e907d
David <deh0_@hotmail.com>
2020-12-08 16:57:48 -0300

    Adicionado implementação da interface Tableobject e criado método statico que retorna os nomes das colunas para os jtables

--- src/main/java/br/com/david/testeinsight/model/AbstractModel.java
+++ src/main/java/br/com/david/testeinsight/model/AbstractModel.java
@@ -1,17 +1,41 @@
 package br.com.david.testeinsight.model;
 
+import br.com.david.testeinsight.table.TableObject;
 import java.time.LocalTime;
 
 /**
- *Classe abstrata para as models
+ * Classe abstrata para as models
+ *
  * @author David Arruda
  */
-public abstract class AbstractModel {
-    
+public abstract class AbstractModel implements TableObject{
+
     protected LocalTime entryTime;
-    
+
     protected LocalTime departureTime;
 
+    @Override
+    public Object value(int column) {   
+        switch (column) {
+            case 0:
+                return entryTime.toString();
+            case 1:
+                return departureTime.toString();
+            default: 
+                return" - ";
+        }
+    }
+    
+    /**
+     * Retorna um array com os nomes das colunas que serão apresentadas
+     * @return Object[]
+     */
+    static public Object[] getColumns() {
+        return new String[]{"Entrada","Saída"};
+    }
+    
+    
+    // Getters e Setters
     public LocalTime getEntryTime() {
         return entryTime;
     }
@@ -27,5 +51,12 @@ public abstract class AbstractModel {
     public void setDepartureTime(LocalTime departureTime) {
         this.departureTime = departureTime;
     }
+
+    //ToString
+    @Override
+    public String toString() {
+        return entryTime + "," + departureTime;
+    }
+   
     
 }
