353b2e2878b46e036c706aa7dd1398ad549ae9b9
David <deh0_@hotmail.com>
2020-12-08 15:33:25 -0300

    Criado classe App

---
+++ src/main/java/br/com/david/testeinsight/App.java
@@ -0,0 +1,12 @@
+package br.com.david.testeinsight;
+
+/**
+ *Classe principal
+ * @author David Arruda
+ */
+public class App {
+    
+    public static void main(String[] args) {
+        
+    }
+}
