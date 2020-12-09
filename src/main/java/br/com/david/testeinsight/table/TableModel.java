b3cd9a13f1b5d98778f36dc2eac035a8e96cabd7
David <deh0_@hotmail.com>
2020-12-08 21:02:46 -0300

    Movido para outro package

---
+++ src/main/java/br/com/david/testeinsight/table/TableModel.java
@@ -0,0 +1,63 @@
+package br.com.david.testeinsight.table;
+
+import br.com.david.testeinsight.table.TableObject;
+import java.util.LinkedList;
+import java.util.List;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ * Classe reponsável por permitir manipular as listas de objetos(OverTime,
+ * HoursDalay, etc)
+ *
+ * @author David Arruda
+ */
+public class TableModel extends DefaultTableModel {
+
+    private final List<TableObject> list;
+
+    /**
+     * Construtor. Retornar um TableModel com uma lista de objetos e a descrição
+     * das colunas que serão apresentadas
+     *
+     * @param list
+     * @param colunms
+     */
+    public TableModel(LinkedList list, Object[] colunms) {
+        super(colunms, 0);
+        this.list = list;
+    }
+
+    /**
+     * Exibe os dados na tabela Jtable. Ultiliza o método value definido na
+     * interface TableObjetc
+     *
+     * @param row
+     * @param column
+     * @return
+     */
+    @Override
+    public Object getValueAt(int row, int column) {
+        return list.get(row).value(column);
+    }
+
+    /**
+     * Define o número de elementos na tabela
+     *
+     * @return int
+     */
+    @Override
+    public int getRowCount() {
+        return list != null ? list.size() : 0;
+    }
+
+    /**
+     * Recupera os dados completos de um objeto da lista.
+     *
+     * @param row
+     * @return TableObject
+     */
+    public TableObject getValue(int row) {
+        return list.get(row);
+    }
+
+}
