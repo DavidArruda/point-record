7d0bf81a07ac15c499cae0a906e52c910011f2a4
David <deh0_@hotmail.com>
2020-12-14 20:10:35 -0300

    Adicionado composição com a classe WorkingHours

--- src/main/java/br/com/david/testeinsight/model/MarkingMade.java
+++ src/main/java/br/com/david/testeinsight/model/MarkingMade.java
@@ -4,6 +4,16 @@ package br.com.david.testeinsight.model;
  *
  * @author David Arruda
  */
-public class MarkingMade extends AbstractModel{
-    
+public class MarkingMade extends AbstractModel {
+
+    private WorkingHours workingHours;
+
+    public WorkingHours getWorkingHours() {
+        return workingHours;
+    }
+
+    public void setWorkingHours(WorkingHours workingHours) {
+        this.workingHours = workingHours;
+    }
+
 }
