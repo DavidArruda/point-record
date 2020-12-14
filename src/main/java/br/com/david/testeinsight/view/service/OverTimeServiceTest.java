8405d85f00c6c680666e655a1bf1ebc5e085bdb6
David <deh0_@hotmail.com>
2020-12-14 19:50:11 -0300

    Criado service para calculo de horas extra

---
+++ src/main/java/br/com/david/testeinsight/view/service/OverTimeServiceTest.java
@@ -0,0 +1,10 @@
+package br.com.david.testeinsight.view.service;
+
+/**
+ * Service para responsável pelas horas extra
+ *
+ * @author David Arruda
+ */
+public interface OverTimeServiceTest extends CalculateHoursService {
+
+}
