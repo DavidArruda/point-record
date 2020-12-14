64c6e3fbbd209afd9ba6884d6f9752b14f2b9516
David <deh0_@hotmail.com>
2020-12-14 19:49:55 -0300

    Criado service para calculo de horas de atraso

--- src/main/java/br/com/david/testeinsight/view/service/HoursDelayService.java
+++ src/main/java/br/com/david/testeinsight/view/service/HoursDelayService.java
@@ -1,23 +1,10 @@
 package br.com.david.testeinsight.view.service;
 
-import javax.swing.JTable;
-
 /**
  * Service para responsável pelas horas de atraso
  * @author David Arruda
  */
-public interface HoursDelayService {
+public interface HoursDelayService extends CalculateHoursService{
 
-    /**
-     * Calcula as horas de atraso com base na jornada de trabalho e as marcações
-     * feitas.
-     *
-     * @param workingHours
-     * @param markingMade
-     * @param hoursDelay
-     * @return Jtable
-     * @throws Exception
-     */
-    JTable delayHours(JTable workingHours, JTable markingMade, JTable hoursDelay) throws Exception;
 
 }
