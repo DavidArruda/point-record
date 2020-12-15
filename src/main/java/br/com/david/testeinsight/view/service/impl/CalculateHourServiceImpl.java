e4d19a2aa43ac93fbc4bdd9ea1de8281c533f1d1
David <deh0_@hotmail.com>
2020-12-15 20:57:36 -0300

    Removido métodos abstratos

--- src/main/java/br/com/david/testeinsight/view/service/impl/CalculateHourServiceImpl.java
+++ src/main/java/br/com/david/testeinsight/view/service/impl/CalculateHourServiceImpl.java
@@ -1,22 +1,21 @@
 package br.com.david.testeinsight.view.service.impl;
 
+import java.lang.reflect.Method;
+import java.util.LinkedList;
+
+import javax.swing.JTable;
+import javax.swing.table.TableModel;
+
 import br.com.david.testeinsight.model.MarkingMade;
 import br.com.david.testeinsight.model.WorkingHours;
 import br.com.david.testeinsight.table.TableObject;
 import br.com.david.testeinsight.view.service.CalculateHoursService;
-import java.lang.reflect.Method;
-import java.time.LocalTime;
-import java.util.LinkedList;
-import javax.swing.JTable;
 
 /**
  *
  * @author David Arruda
  */
 public abstract class CalculateHourServiceImpl implements CalculateHoursService {
-
-    @Override
-    public abstract void subtractBetweenHours(JTable workingHours, JTable markingMade, JTable jTable) throws Exception;
 
     @Override // modificar método recever JTable
     public int subtractPeriods(WorkingHours workingHours, MarkingMade markingMade) {
@@ -29,24 +28,23 @@ public abstract class CalculateHourServiceImpl implements CalculateHoursService 
         return hoursWorked - programmedHours;
     }
 
-    @Override
-    public abstract LocalTime[] getPeriod(WorkingHours workingHours, MarkingMade markingMade);
-
-
-    @Override
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+	@Override
     public LinkedList getList(JTable workingHours) throws Exception {
-        Class tableModel = workingHours.getModel().getClass(); // Obtem o tipo da classe
+        Class<? extends TableModel> tableModel = workingHours.getModel().getClass(); // Obtem o tipo da classe
         Method method = tableModel.getMethod("getList"); // Obtem o método através de sua assinatura
         LinkedList list = (LinkedList) method.invoke(workingHours.getModel()); // Invoca o método passando de qual classe ele será chamado
         return list;
     }
 
 
+    //Corrigir método
     @Override
     public void addRowTable(JTable jTable, TableObject object) throws Exception {
         Class tableModel = jTable.getModel().getClass(); // Obtem o tipo da classe
-        Method method = tableModel.getMethod("addRow", object.getClass()); // Obtem o método através de sua assinatura
-        method.invoke(jTable.getModel(), object); // Invoca o método passando de qual classe ele será chamado e seus parametros, respectivamente
+		Method method = tableModel.getMethod("addRowTest", new Class[] {object.getClass()}); // Obtem o método através de sua assinatura
+        method.setAccessible(true);
+		method.invoke(jTable.getModel(), new TableObject[] {object}); // Invoca o método passando de qual classe ele será chamado e seus parametros, respectivamente
     }
 
 }
