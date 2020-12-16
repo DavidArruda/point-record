9fd1e2c6f6fed6f0785f9d57004353adf95c4252
David <deh0_@hotmail.com>
2020-12-15 21:03:24 -0300

    Adicionado campo id

--- src/main/java/br/com/david/testeinsight/model/MarkingMade.java
+++ src/main/java/br/com/david/testeinsight/model/MarkingMade.java
@@ -6,7 +6,25 @@ package br.com.david.testeinsight.model;
  */
 public class MarkingMade extends AbstractModel {
 
-    private WorkingHours workingHours;
+    private WorkingHours workingHours = new WorkingHours();
+
+    @Override
+    public Object value(int column) {   
+        switch (column) {
+            case 0:
+                return entryTime.toString();
+            case 1:
+                return departureTime.toString();
+            case 2:
+                return workingHours.getId().toString();
+            default: 
+                return" - ";
+        }
+    }
+
+    static public Object[] getColumnsMarkingMade() {
+        return new String[]{"Entrada","Saída", "Jornada de Trabalho"};
+    }
 
     public WorkingHours getWorkingHours() {
         return workingHours;
