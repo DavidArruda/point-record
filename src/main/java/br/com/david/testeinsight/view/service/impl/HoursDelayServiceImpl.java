86e2c3f7103184b81831bace9a5d5b1593bc5baa
David <deh0_@hotmail.com>
2020-12-15 21:01:00 -0300

    Corrido método period, falta terminar a implementação para pegar todos os periodos corretamente

--- src/main/java/br/com/david/testeinsight/view/service/impl/HoursDelayServiceImpl.java
+++ src/main/java/br/com/david/testeinsight/view/service/impl/HoursDelayServiceImpl.java
@@ -14,36 +14,58 @@ import javax.swing.JTable;
  */
 public class HoursDelayServiceImpl extends CalculateHourServiceImpl implements HoursDelayService {
 
-    @Override
-    public void subtractBetweenHours(JTable workingHours, JTable markingMade, JTable tbHoursDelay) throws Exception {
+    @SuppressWarnings("unchecked")
+	@Override
+    public HoursDelay subtractBetweenHours(JTable workingHours, JTable markingMade, JTable tbHoursDelay) throws Exception {
         LinkedList<WorkingHours> listWorkingHours = getList(workingHours);
         LinkedList<MarkingMade> listMKMades = getList(markingMade);
 
-        var jornadaTrabalho = listWorkingHours.get(0);
-        var marcacaoFeita = listMKMades.get(0);
+        var jornadaTrabalho = listWorkingHours.getLast();
+        var marcacaoFeita = listMKMades.getLast();
 
         LocalTime[] entryAndDeparture = getPeriod(jornadaTrabalho, marcacaoFeita);
 
         HoursDelay hoursDelay = new HoursDelay();
         hoursDelay.setEntryTime(entryAndDeparture[0]);
         hoursDelay.setDepartureTime(entryAndDeparture[1]);
+        
+        /*if (entryAndDeparture.length == 4) {
+        	HoursDelay hoursDelay2 = new HoursDelay();
+        	hoursDelay2.setEntryTime(entryAndDeparture[2]);
+            hoursDelay2.setDepartureTime(entryAndDeparture[3]);
+		}*/
 
-        addRowTable(tbHoursDelay, hoursDelay);
+        return hoursDelay;
     }
 
     @Override
     public LocalTime[] getPeriod(WorkingHours workingHours, MarkingMade markingMade) {
+    	//LinkedList<LocalTime> delays = new LinkedList<>();
+    	
         if (markingMade.getEntryTime().isAfter(workingHours.getEntryTime())) {
             LocalTime entry = LocalTime.of(workingHours.getEntryTime().getHour(), workingHours.getEntryTime().getMinute());
             LocalTime departure = LocalTime.of(markingMade.getEntryTime().getHour(), markingMade.getEntryTime().getMinute());
-
+            
+            /*
+            delays.add(entry);
+            delays.add(departure);
+            
+            if (markingMade.getDepartureTime().isBefore(workingHours.getDepartureTime())) {
+				LocalTime entry2 = LocalTime.of(markingMade.getDepartureTime().getHour(),markingMade.getDepartureTime().getMinute());
+				LocalTime departure2 = LocalTime.of(workingHours.getDepartureTime().getHour(), workingHours.getDepartureTime().getMinute());
+				
+				delays.add(entry2);
+				delays.add(departure2);
+			}*/
+            
             LocalTime[] period = {entry, departure};
 
             return period;
 
         } else {
-            LocalTime entry = LocalTime.of(markingMade.getEntryTime().getHour(), markingMade.getEntryTime().getMinute());
-            LocalTime departure = LocalTime.of(workingHours.getEntryTime().getHour(), workingHours.getEntryTime().getMinute());
+        	
+            LocalTime entry = LocalTime.of(markingMade.getDepartureTime().getHour(), markingMade.getDepartureTime().getMinute());
+            LocalTime departure = LocalTime.of(workingHours.getDepartureTime().getHour(), workingHours.getDepartureTime().getMinute());
 
             LocalTime[] period = {entry, departure};
 
