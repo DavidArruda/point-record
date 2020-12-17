7d3f73ff630fd5006d7fc6d92cda0e4a62d25764
David <deh0_@hotmail.com>
2020-12-16 22:56:49 -0300

    Corrigido service de cálculo de hora, foi alterado o método getPeriod, o ifs agoras são ifs simples ou seja verificara cada condição.
    E foi alterado o método subtractBetweenHours para que o mesmo insira os
    objetos diretamento no Jtable de Horas de atrasos

--- src/main/java/br/com/david/testeinsight/service/impl/HoursDelayServiceImpl.java
+++ src/main/java/br/com/david/testeinsight/service/impl/HoursDelayServiceImpl.java
@@ -4,7 +4,11 @@ import br.com.david.testeinsight.model.HoursDelay;
 import br.com.david.testeinsight.model.MarkingMade;
 import br.com.david.testeinsight.model.WorkingHours;
 import br.com.david.testeinsight.service.HoursDelayService;
+import br.com.david.testeinsight.table.TableModel;
+import br.com.david.testeinsight.table.TableObject;
+
 import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import javax.swing.JTable;
 
@@ -14,63 +18,75 @@ import javax.swing.JTable;
  */
 public class HoursDelayServiceImpl extends CalculateHourServiceImpl implements HoursDelayService {
 
-    @SuppressWarnings("unchecked")
+	@SuppressWarnings("unchecked")
 	@Override
-    public HoursDelay subtractBetweenHours(JTable workingHours, JTable markingMade, JTable tbHoursDelay) throws Exception {
-        LinkedList<WorkingHours> listWorkingHours = getList(workingHours);
-        LinkedList<MarkingMade> listMKMades = getList(markingMade);
+	public void subtractBetweenHours(JTable workingHours, JTable markingMade, TableModel tbHoursDelay)
+			throws Exception {
+		LinkedList<WorkingHours> listWorkingHours = getList(workingHours);
+		LinkedList<MarkingMade> listMKMades = getList(markingMade);
 
-        var jornadaTrabalho = listWorkingHours.getLast();
-        var marcacaoFeita = listMKMades.getLast();
+		var jornadaTrabalho = listWorkingHours.getLast();
+		var marcacaoFeita = listMKMades.getLast();
 
-        LocalTime[] entryAndDeparture = getPeriod(jornadaTrabalho, marcacaoFeita);
+		LocalTime[] entryAndDeparture = getPeriod(jornadaTrabalho, marcacaoFeita);
 
-        HoursDelay hoursDelay = new HoursDelay();
-        hoursDelay.setEntryTime(entryAndDeparture[0]);
-        hoursDelay.setDepartureTime(entryAndDeparture[1]);
-        
-        /*if (entryAndDeparture.length == 4) {
-        	HoursDelay hoursDelay2 = new HoursDelay();
-        	hoursDelay2.setEntryTime(entryAndDeparture[2]);
-            hoursDelay2.setDepartureTime(entryAndDeparture[3]);
-		}*/
+		HoursDelay hoursDelay = new HoursDelay();
+		hoursDelay.setEntryTime(entryAndDeparture[0]);
+		hoursDelay.setDepartureTime(entryAndDeparture[1]);
 
-        return hoursDelay;
-    }
+		// ADICIONA NO JTABLE DE HORAS DE ATRASO
+		tbHoursDelay.addRow(hoursDelay);
 
-    @Override
-    public LocalTime[] getPeriod(WorkingHours workingHours, MarkingMade markingMade) {
-    	//LinkedList<LocalTime> delays = new LinkedList<>();
-    	
-        if (markingMade.getEntryTime().isAfter(workingHours.getEntryTime())) {
-            LocalTime entry = LocalTime.of(workingHours.getEntryTime().getHour(), workingHours.getEntryTime().getMinute());
-            LocalTime departure = LocalTime.of(markingMade.getEntryTime().getHour(), markingMade.getEntryTime().getMinute());
-            
-            /*
-            delays.add(entry);
-            delays.add(departure);
-            
-            if (markingMade.getDepartureTime().isBefore(workingHours.getDepartureTime())) {
-				LocalTime entry2 = LocalTime.of(markingMade.getDepartureTime().getHour(),markingMade.getDepartureTime().getMinute());
-				LocalTime departure2 = LocalTime.of(workingHours.getDepartureTime().getHour(), workingHours.getDepartureTime().getMinute());
-				
-				delays.add(entry2);
-				delays.add(departure2);
-			}*/
-            
-            LocalTime[] period = {entry, departure};
+		// VERIFICA SE EXISTE MAIS HORAS DE ATRASO
+		if (entryAndDeparture.length == 4) {
+			HoursDelay hoursDelay2 = new HoursDelay();
+			hoursDelay2.setEntryTime(entryAndDeparture[2]);
+			hoursDelay2.setDepartureTime(entryAndDeparture[3]);
 
-            return period;
+			tbHoursDelay.addRow(hoursDelay2);
+		}
 
-        } else {
-        	
-            LocalTime entry = LocalTime.of(markingMade.getDepartureTime().getHour(), markingMade.getDepartureTime().getMinute());
-            LocalTime departure = LocalTime.of(workingHours.getDepartureTime().getHour(), workingHours.getDepartureTime().getMinute());
+	}
 
-            LocalTime[] period = {entry, departure};
+	@Override
+	public LocalTime[] getPeriod(WorkingHours workingHours, MarkingMade markingMade) {
+		ArrayList<LocalTime> delays = new ArrayList<>();
 
-            return period;
-        }
-    }
+		if (markingMade.getEntryTime().isAfter(workingHours.getEntryTime())) {
+			LocalTime entry = LocalTime.of(workingHours.getEntryTime().getHour(),
+					workingHours.getEntryTime().getMinute());
+			LocalTime departure = LocalTime.of(markingMade.getEntryTime().getHour(),
+					markingMade.getEntryTime().getMinute());
+
+			delays.add(entry);
+			delays.add(departure);
+
+			/*
+			 * if (markingMade.getDepartureTime().isBefore(workingHours.getDepartureTime()))
+			 * { LocalTime entry2 = LocalTime.of(markingMade.getDepartureTime().getHour(),
+			 * markingMade.getDepartureTime().getMinute()); LocalTime departure2 =
+			 * LocalTime.of(workingHours.getDepartureTime().getHour(),
+			 * workingHours.getDepartureTime().getMinute());
+			 * 
+			 * LocalTime[] period = { entry, departure, entry2, departure2 }; return period;
+			 * }
+			 * 
+			 * LocalTime[] period = { entry, departure }; return period;
+			 */
+		}
+
+		if (markingMade.getDepartureTime().isBefore(workingHours.getDepartureTime())) {
+
+			LocalTime entry = LocalTime.of(markingMade.getDepartureTime().getHour(),
+					markingMade.getDepartureTime().getMinute());
+			LocalTime departure = LocalTime.of(workingHours.getDepartureTime().getHour(),
+					workingHours.getDepartureTime().getMinute());
+
+			delays.add(entry);
+			delays.add(departure);
+		}
+
+		return delays.toArray(new LocalTime[delays.size()]);
+	}
 
 }
