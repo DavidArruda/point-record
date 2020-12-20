19f0af066ec408a8fcf3d82240b6c82580ab6e3d
David <deh0_@hotmail.com>
2020-12-20 16:59:46 -0300

    Implementado métodos para calcular horas pendentes

--- src/main/java/br/com/david/testeinsight/service/impl/CalculateHourServiceImpl.java
+++ src/main/java/br/com/david/testeinsight/service/impl/CalculateHourServiceImpl.java
@@ -8,7 +8,6 @@ import javax.swing.table.TableModel;
 
 import br.com.david.testeinsight.model.MarkingMade;
 import br.com.david.testeinsight.model.WorkingHours;
-import br.com.david.testeinsight.table.TableObject;
 import br.com.david.testeinsight.service.CalculateHoursService;
 
 /**
@@ -17,34 +16,57 @@ import br.com.david.testeinsight.service.CalculateHoursService;
  */
 public abstract class CalculateHourServiceImpl implements CalculateHoursService {
 
-    @Override // modificar método recever JTable
-    public int subtractPeriods(WorkingHours workingHours, MarkingMade markingMade) {
-        //  Subtrai saida - entrada
-        int programmedHours = workingHours.getDepartureTime().getHour() - workingHours.getEntryTime().getHour();
+	@Override // modificar método recever JTable
+	public int subtractPeriods(WorkingHours workingHours, MarkingMade markingMade) {
+		// Subtrai saida - entrada
+		int programmedHours = workingHours.getDepartureTime().getHour() - workingHours.getEntryTime().getHour();
 
-        // Subtrai saida - entrada
-        int hoursWorked = markingMade.getDepartureTime().getHour() - markingMade.getEntryTime().getHour();
+		// Subtrai saida - entrada
+		int hoursWorked = markingMade.getDepartureTime().getHour() - markingMade.getEntryTime().getHour();
 
-        return hoursWorked - programmedHours;
-    }
+		return hoursWorked - programmedHours;
+	}
 
-    @SuppressWarnings({ "rawtypes", "unchecked" })
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	@Override
-    public LinkedList getList(JTable workingHours) throws Exception {
-        Class<? extends TableModel> tableModel = workingHours.getModel().getClass(); // Obtem o tipo da classe
-        Method method = tableModel.getMethod("getList"); // Obtem o método através de sua assinatura
-        LinkedList list = (LinkedList) method.invoke(workingHours.getModel()); // Invoca o método passando de qual classe ele será chamado
-        return list;
-    }
+	public LinkedList getList(JTable workingHours) throws Exception {
+		Class<? extends TableModel> tableModel = workingHours.getModel().getClass(); // Obtem o tipo da classe
+		Method method = tableModel.getMethod("getList"); // Obtem o método através de sua assinatura
+		LinkedList list = (LinkedList) method.invoke(workingHours.getModel()); // Invoca o método passando de qual
+																				// classe ele será chamado
+		return list;
+	}
+	
+	@Override
+	public int calcPendingHoursDeparture(WorkingHours workingHours, MarkingMade markingMade) {
+		int hoursPending;
+		
+		// SE A SAIDA DA MARCAÇÃO FOR ANTES DA SAIDA DO HORÁRIO DE TRABALHO
+		if (markingMade.getDepartureTime().isBefore(workingHours.getDepartureTime())) {
+			hoursPending = markingMade.getDepartureTime().getHour() - workingHours.getDepartureTime().getHour();
+		
+		} else {
+			hoursPending = workingHours.getDepartureTime().getHour() - markingMade.getDepartureTime().getHour();
+		}
+		
+		return hoursPending;
+	}
+	
+	@Override
+	public int calcPendingHoursEntry(WorkingHours workingHours, MarkingMade markingMade) {
+		int hoursPending;
 
+		// SE A ENTRADA DA MARCAÇÃO FOR APÓS A ENTRADA DO HORÁRIO DE TRABALHO
+		if (markingMade.getEntryTime().isAfter(workingHours.getEntryTime())) {
+			hoursPending = markingMade.getEntryTime().getHour() - workingHours.getEntryTime().getHour();
+		
+		} else {
+			hoursPending = workingHours.getEntryTime().getHour() - markingMade.getEntryTime().getHour();
+		}
 
-    //Corrigir método
-    @Override
-    public void addRowTable(JTable jTable, TableObject object) throws Exception {
-        Class tableModel = jTable.getModel().getClass(); // Obtem o tipo da classe
-		Method method = tableModel.getMethod("addRowTest", new Class[] {object.getClass()}); // Obtem o método através de sua assinatura
-        method.setAccessible(true);
-		method.invoke(jTable.getModel(), new TableObject[] {object}); // Invoca o método passando de qual classe ele será chamado e seus parametros, respectivamente
-    }
+		return hoursPending;
+	}
+	
+	
 
 }
